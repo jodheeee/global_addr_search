0da4a32793a0c6dfcb75526b06519966043740a5
Dae-Hee <75158767+jodheeee@users.noreply.github.com>
2021-11-30 13:46:13 +0900

    Create Index.tsx

---
+++ Index.tsx
@@ -0,0 +1,22 @@
+import React,{ useState } from 'react';
+import AddrModal from '../item/AddrModal';
+
+function Index() {
+    const [modalSet, setModalSet] = useState(false);
+    const [zipCode, setZipCode] = useState('');
+    const [addr, setAddr] = useState('');
+
+    return (
+        <div className="Index">
+            <a onClick={()=>{
+                setModalSet(true);
+            }}>검색</a>
+            <h1>우편번호 : {zipCode}</h1>
+            <h1>주소 : {addr}</h1>
+            <AddrModal open={modalSet} reSet={setModalSet} setZipCode={setZipCode} setAddr={setAddr} />
+        </div>
+    );
+    
+}
+
+export default Index;
